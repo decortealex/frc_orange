a8e958f41c6a086313e33ea06237f329095e5380
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-12-04 02:16:48 -0500

    button pressed

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+import json
 
 app = Flask(__name__)
 
@@ -9,16 +9,33 @@ def index():
     return render_template('index.html')
 
 
+def json_parse(data):
+    data = json.dumps(data)
+    data = json.loads(data)
+    return(data)
 @app.route("/test", methods=["POST"])
 def test():
-    
     clicked=None
     if request.method == "POST":
-        
         data = request.get_json()
-        print(data)
+        data = json_parse(data)
+        print(data['data'])
+    return "nothing"
+@app.route("/dropdown", methods=["POST"])
+def dropdown():
+    clicked=None
+    if request.method == "POST":
+        data = request.get_json()
+        data = json_parse(data)
+        print(data['dropdown'])
+    return "nothing"
 
-        
+@app.route('/button')
+def button():
+    print('button pressed')
+    return "Nothing"
+
+
     
     return render_template('index.html')
 
